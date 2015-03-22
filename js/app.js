61e33acffe8eb702ed9eb5df1b94d1d0361571d9
Deepanjan Roy <deepanjan.roy@gmail.com>
2015-03-22 18:44:08 -0400

    test

--- js/app.js
+++ js/app.js
@@ -14,32 +14,73 @@ $(document).ready(function(){
 
 var m ;
   	$(".new").click(function(){
+  		  		
+  		$('#userGuess').prop("disabled", false);
+  		
+  		$('#guessButton').prop("disabled", false);
+  		$('#guessList').empty();
+  		$('#count').html("0");
+
+
 
   	m =	Math.floor(Math.random() * (100 - 1)) + 1;
 
   	console.log(m);
 
   	});
+  	
+  	var c=0;
 
-  	$("#guessButton").click(function(){
+ $("#guessButton").click(function(){
 
-  		var v = $("#userGuess").val();
+  	var v = $("#userGuess").val(); // assigning variable "v" to the value given by the user.
+  
+  c=c+1;
+
+   
+  $('#count').html(c);
+
+  console.log(c);    
+
+   var low1 = m-50;
+   var high1 = m+50; 
+
+   var low2 = m-40;
+   var high2= m+40;
+
+   var low3 = m-30;
+   var high3 = m+30;
+
+   var low4 = m-20;
+   var high4 = m+20;
+
+   var low5 = m-10;
+   var high5 = m+10;
+
+$('#guessList')
+
+	if (v==m) {$('#guessList').append("You are correct"+'<br>');}
+
+	else if (v >=low5 && v<=high5) {$('#guessList').append(v+" -very Hot"+'<br>');}
+
+	else if (v >=low4 && v<=high4) {$('#guessList').append(v+"-Hot"+'<br>');}
+
+	else if (v >=low3 && v<=high3) {$('#guessList').append(v+"-Warm"+'<br>');}
+
+	else if (v >=low2 && v<=high2) {$('#guessList').append(v+"-Cold"+'<br>');}
+
+	else if (v >=low1 && v<=high1) {$('#guessList').append(v+"-Ice Cold"+'<br>');}
+
+if (c==3) {alert("You are out of turns. Start new game");
+
+$('#userGuess').prop("disabled", true); 
+$('#guessButton').prop("disabled", true);
+
+  		}
+return false;
+
+
+
+
   	});
-
-
-{
-	if (v=m) {alert("You are correct");}
-
-	else if (v = (m<=50 || m>=50)) {alert("Ice Cold");}
-
-	else if (v = (m<=40 || m>=40)) {alert("Very Hot");}
-
-	else if (v = (m<=30 || m>=30)) {alert("Warm");}
-
-	else if (v = (m<=20 || m>=20)) {alert("Hot");}
-
-	else if (v = (m<=10 || m>=10)) {alert("Very Hot");}
-
-
-};
 });
